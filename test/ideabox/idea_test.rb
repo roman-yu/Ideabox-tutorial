3e78599c913b5197d923d75460d5debea1f4604c
Roman YU <romanyu827@gmail.com>
2016-02-22 18:53:23 +0800

    Extracting an ‘Around’ Method.

--- test/ideabox/idea_test.rb
+++ test/ideabox/idea_test.rb
@@ -9,15 +9,20 @@ class CreateIdeas < ActiveRecord::Migration
 end
 
 begin
-	CreateIdeas.new.change
+  CreateIdeas.new.change
 rescue ActiveRecord::StatementInvalid
 end
 
 class IdeaTest < MiniTest::Unit::TestCase
+  include WithRollback
 
   def test_it_exists
-    idea = Idea.new(:description => 'A wonderful idea!')
-    assert_equal 'A wonderful idea!', idea.description
+    assert_equal 0, Idea.count
+    temporarily do
+      Idea.create(:description => 'A wonderful idea!')
+      assert_equal 1, Idea.count
+    end
+    assert_equal 0, Idea.count
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,12 @@ $:.unshift File.expand_path("./../../lib", __FILE__)
 
 require './config/environment'
 require 'minitest/autorun'
+
+module WithRollback
+  def temporarily(&block)
+    ActiveRecord::Base.connection.transaction do
+      block.call
+      raise ActiveRecord::Rollback
+    end
+  end
+end
